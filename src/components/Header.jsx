e0c00a6eed6c742dd7397d517d92a50d39752b24
OmodaraAyo <ayodeleomodara1234@gmail.com>
2025-04-28 21:45:31 +0100

    updated... sign up page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import images from "../assets/images";
 import ShinyText from "./ShinyText.jsx";
 import clsx from "clsx";
+import Login from "../pages/Login.jsx";
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [openSignUp, setOpenSignIn] = useState(false);
+
   const navBarItems = [
     { page: "Home", path: "/" },
     { page: "Pricing", path: "/pricing" },
@@ -12,8 +16,21 @@ const Header = () => {
     { page: "FAQs", path: "/faqs" },
     { page: "Doc", path: "/documentation" },
   ];
+
+  useEffect(()=> {
+
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    
   return (
-    <header className="border-b-2 ">
+    <header className={clsx(`border-b-2 sticky top-0 z-40 transition-all duration-300 `, isScrolled && "bg-[#07020D] backdrop-blur-lg backdrop-brightness-110")}>
       <div className="container mx-auto h-full flex justify-between items-center px-4 py-3 ">
         <div>
           <Link to={"/"}>
@@ -44,18 +61,13 @@ const Header = () => {
           ))}
         </nav>
 
-        <NavLink
-          className={({ isActive }) =>
-            clsx(
-              "py-1 px-5 font-semibold text-md border border-purple-600 transform -skew-x-14 rounded hover:bg-purple-600",
-              isActive && "bg-purple-600"
-            )
-          }
-          to={"sign-in"}
-        >
+        <button
+          onClick={() => setOpenSignIn(true)}
+          className="py-1 px-5 font-semibold text-md border border-purple-600 transform -skew-x-14 rounded hover:bg-purple-600 cursor-pointer">
           Get Started
-        </NavLink>
+        </button>
       </div>
+      {openSignUp && <Login onClose={() => setOpenSignIn(false)} />}
     </header>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,46 +2,39 @@ import React from "react";
 import Particles from "../components/Particles";
 import { Link } from "react-router-dom";
 import ShinyText from "../components/ShinyText";
+import Login from "./Login";
 
 const Home = () => {
   return (
-    <div className="relative w-full h-full overflow-hidden">
-      <div style={{ width: "100%", height: "600px", position: "relative" }}>
-        <Particles
-          particleColors={["#ffffff", "#ffffff"]}
-          particleCount={200}
-          particleSpread={10}
-          speed={0.1}
-          particleBaseSize={100}
-          moveParticlesOnHover={false}
-          alphaParticles={false}
-          disableRotation={false}
-        />
-      </div>
-
-      <div className="absolute top-0 left-0 right-0 bottom-0 w-full h-full pt-36">
-        
-        <div className="container mx-auto flex flex-col items-center py-3">
-
-          <h1 className="w-full lg:max-w-10/12 text-center text-[2.30rem] font-semibold tracking-wider leading-14">Where <span className="text-[#10041e] font-bold bg-purple-600 px-2 rounded-bl-lg rounded-tr-lg">USSD</span> Meets Innovation</h1>
-          <p className="text-center text-[#b5b5b5a4] text-lg">Revolutionizing Business Connectivity with Instant, Reliable, and Hassle-Free USSD Solutions.</p>
-
-          <div className="flex mt-5 w-full justify-center items-center flex-col md:flex-row gap-5 md:gap-8 font-semibold text-md">
-            <Link className="py-2 px-36 bg-white text-[#07020D]  rounded-lg hover:bg-purple-700 hover:text-white transition duration-300 ease-in-out -skew-x-14">Book a demo</Link>
-            <Link className="py-2 px-10 border border-purple-600 rounded-lg hover:bg-purple-700 transition duration-300 ease-in-out -skew-x-14" to={"sign-in"}>
-                        <ShinyText
-                          text="Try it Now"
-                          disabled={false}
-                          speed={3}
-                          className="custom-class"
-                        />
-                    </Link>
+    <section>
+      <div className="relative w-full h-full overflow-hidden">
+          <div style={{ width: "100%", height: "600px", position: "relative" }}>
+              <Particles
+                particleColors={["#ffffff", "#ffffff"]}
+                particleCount={200}
+                particleSpread={10}
+                speed={0.1}
+                particleBaseSize={100}
+                moveParticlesOnHover={false}
+                alphaParticles={false}
+                disableRotation={false}
+              />
           </div>
 
+          <div className="absolute top-0 left-0 right-0 bottom-0 w-full h-full pt-36">
+            <div className="container mx-auto flex flex-col items-center py-3">
 
-        </div>
+              <h1 className="w-full lg:max-w-10/12 text-center text-[2.30rem] font-semibold tracking-wider leading-14">Where <span className="text-[#10041e] font-bold bg-purple-600 px-2 rounded-bl-lg rounded-tr-lg">USSD</span> Meets Innovation</h1>
+              <p className="text-center text-[#b5b5b5a4] text-lg">Revolutionizing Business Connectivity with Instant, Reliable, and Hassle-Free USSD Solutions.</p>
+
+              <div className="flex mt-5 w-full justify-center items-center flex-col md:flex-row gap-5 md:gap-8 font-semibold text-md">
+                <Link className="py-2 px-36 bg-white text-[#07020D]  rounded-lg hover:bg-purple-700 hover:text-white transition duration-300 ease-in-out -skew-x-14">Book a demo</Link>
+                <Link className="py-2 px-10 border border-purple-600 rounded-lg hover:bg-purple-700 transition duration-300 ease-in-out -skew-x-14" to={"sign-in"}><ShinyText text="Try it Now" disabled={false} speed={3} className="custom-class"/></Link>
+              </div>
+            </div>
+          </div>
       </div>
-    </div>
+    </section>
   );
 };
 
