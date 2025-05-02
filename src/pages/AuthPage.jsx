c45020045ffeac5085360e70c7c2abbadf1e644a
OmodaraAyo <ayodeleomodara1234@gmail.com>
2025-05-02 12:16:06 +0100

    h update'

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -15,7 +15,7 @@ const AuthPage = ({ onClose, showLogin}) => {
     }, []);
 
   return (
-    <section id="authentication" className="fixed inset-0 w-full bg-black/60 z-50 flex justify-end">
+    <section id="authentication" className="fixed inset-0 w-full h-[100dvh] bg-black/60 z-50 flex justify-end">
       <div className="flex flex-col ml-auto bg-[#f5f3f3] w-full sm:max-w-xl h-full rounded-lg p-5 md:p-10 overflow-y-auto scrollbar-custom">
         <button onClick={onClose} className="block ml-auto text-[#07020D] hover:text-black/100 transition-all duration-300 ease-in-out cursor-pointer">
           <IoMdClose size={32} />
